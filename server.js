f808d33d7bb916c6e022e0082c475198cfbf5665
Adam Bjurstrom <adam.bjurstrom@torchtechnologies.com>
2022-05-04 12:44:29 -0500

    Refactored to use express server, yarn, prettier, etc.

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+app.use("/dist", express.static("dist"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(port, () => {
+  console.log("Server is running at http://localhost:" + port);
+});
--- src/UserInterface.ts
+++ src/UserInterface.ts
@@ -1,6 +1,6 @@
-export default interface User{
-    name: string;
-    age: number;
-    address: string;
-    dob: Date;
-  }
+export default interface User {
+  name: string;
+  age: number;
+  address: string;
+  dob: Date;
+}
